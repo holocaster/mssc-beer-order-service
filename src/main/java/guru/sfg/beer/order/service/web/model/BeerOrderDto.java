7d1533d71c3247bc3e19ce3a06978fdd08e0201f
Paulo Roberto <paulorobertosp@gmail.com>
2021-04-19 15:27:21 -0300

    Refactor

--- src/main/java/guru/sfg/beer/order/service/web/model/BeerOrderDto.java
+++ src/main/java/guru/sfg/beer/order/service/web/model/BeerOrderDto.java
@@ -17,10 +17,12 @@
 
 package guru.sfg.beer.order.service.web.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import guru.sfg.beer.order.service.domain.enums.BeerOrderStatusEnum;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.time.OffsetDateTime;
@@ -29,8 +31,9 @@ import java.util.UUID;
 
 @Data
 @NoArgsConstructor
-@EqualsAndHashCode(callSuper = true)
-public class BeerOrderDto extends BaseItem {
+@AllArgsConstructor
+@Builder
+public class BeerOrderDto {
 
     private UUID customerId;
     private String customerRef;
@@ -38,14 +41,17 @@ public class BeerOrderDto extends BaseItem {
     private BeerOrderStatusEnum beerOrderStatusEnum;
     private String orderStatusCallbackUrl;
 
-    @Builder
-    public BeerOrderDto(UUID id, Integer version, OffsetDateTime createdDate, OffsetDateTime lastModifiedDate, UUID customerId, List<BeerOrderLineDto> beerOrderLines,
-                        BeerOrderStatusEnum beerOrderStatusEnum, String orderStatusCallbackUrl, String customerRef) {
-        super(id, version, createdDate, lastModifiedDate);
-        this.customerId = customerId;
-        this.beerOrderLines = beerOrderLines;
-        this.beerOrderStatusEnum = beerOrderStatusEnum;
-        this.orderStatusCallbackUrl = orderStatusCallbackUrl;
-        this.customerRef = customerRef;
-    }
+    @JsonProperty("id")
+    private UUID id = null;
+
+    @JsonProperty("version")
+    private Integer version = null;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
+    @JsonProperty("createdDate")
+    private OffsetDateTime createdDate = null;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
+    @JsonProperty("lastModifiedDate")
+    private OffsetDateTime lastModifiedDate = null;
 }
--- src/main/java/guru/sfg/beer/order/service/web/model/BeerOrderLineDto.java
+++ src/main/java/guru/sfg/beer/order/service/web/model/BeerOrderLineDto.java
@@ -17,9 +17,11 @@
 
 package guru.sfg.beer.order.service.web.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
@@ -28,8 +30,9 @@ import java.util.UUID;
 
 @Data
 @NoArgsConstructor
-@EqualsAndHashCode(callSuper = true)
-public class BeerOrderLineDto extends BaseItem {
+@AllArgsConstructor
+@Builder
+public class BeerOrderLineDto {
 
     private String upc;
     private String beerName;
@@ -37,15 +40,17 @@ public class BeerOrderLineDto extends BaseItem {
     private UUID beerId;
     private Integer orderQuantity = 0;
     private BigDecimal price;
-    @Builder
-    public BeerOrderLineDto(UUID id, Integer version, OffsetDateTime createdDate, OffsetDateTime lastModifiedDate,
-                            String upc, String beerName, String beerStyle, UUID beerId, Integer orderQuantity, BigDecimal price) {
-        super(id, version, createdDate, lastModifiedDate);
-        this.upc = upc;
-        this.beerName = beerName;
-        this.beerId = beerId;
-        this.orderQuantity = orderQuantity;
-        this.beerStyle = beerStyle;
-        this.price = price;
-    }
+    @JsonProperty("id")
+    private UUID id = null;
+
+    @JsonProperty("version")
+    private Integer version = null;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
+    @JsonProperty("createdDate")
+    private OffsetDateTime createdDate = null;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
+    @JsonProperty("lastModifiedDate")
+    private OffsetDateTime lastModifiedDate = null;
 }
